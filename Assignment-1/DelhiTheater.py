c027725593f19a9ae513228da460ac04e9315a9d
LOKESH538 <69184455+LOKESH538@users.noreply.github.com>
2020-08-04 20:54:49 +0530

    Update DelhiTheater.py

--- Assignment-1/DelhiTheater.py
+++ Assignment-1/DelhiTheater.py
@@ -1,7 +1,7 @@
-#A theater in Delhi wants to develop a computerized Booking System. The theater offers different types of seats.
-# The Ticket rates are- Stalls- Rs. 625/-, Circle- Rs.750/-, Upper Class- Rs.850/- and Box- Rs.1000/-.
-# A discount is given 10% of total amount if tickets are purchased on Cash.
-# In case of credit card holders 5% discount is given.
+"""A theater in Delhi wants to develop a computerized Booking System. The theater offers different types of seats.
+ The Ticket rates are- Stalls- Rs. 625/-, Circle- Rs.750/-, Upper Class- Rs.850/- and Box- Rs.1000/-.
+ A discount is given 10% of total amount if tickets are purchased on Cash.
+ In case of credit card holders 5% discount is given."""
 seat=input("Type of seat (stalls/circle/upperclass/box):")
 pm=(input("Payment Mode (cash/credit card):"))
 x=[['stalls',625],['circle',750],['upperclass',850],['box',1000]]
