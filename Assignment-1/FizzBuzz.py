3fee5e052de7d7c41293031a1a1becf6a40b9974
LOKESH538 <69184455+LOKESH538@users.noreply.github.com>
2020-08-04 20:53:12 +0530

    Update FizzBuzz.py

--- Assignment-1/FizzBuzz.py
+++ Assignment-1/FizzBuzz.py
@@ -1,6 +1,6 @@
-#Write a Java program which iterates the integers from 1 to 100.
-# For multiples of three print "Fizz" instead of the number and print "Buzz" for the multiples of five.
-# When number is divided by both three and five, print "fizz buzz".
+"""Write a Java program which iterates the integers from 1 to 100.
+ For multiples of three print "Fizz" instead of the number and print "Buzz" for the multiples of five.
+ When number is divided by both three and five, print "fizz buzz"."""
 for i in range(1,101):
     if i % 3 == 0 and i % 5 == 0:
         print("fizzbuzz")
