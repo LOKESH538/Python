5cd7922558fa72d3bab1a7dfa2707d79db7e84d9
LOKESH538 <69184455+LOKESH538@users.noreply.github.com>
2020-08-04 21:23:48 +0530

    Update DelhiTheater.py

--- Assignment-1/DelhiTheater.py
+++ Assignment-1/DelhiTheater.py
@@ -2,6 +2,7 @@
  The Ticket rates are- Stalls- Rs. 625/-, Circle- Rs.750/-, Upper Class- Rs.850/- and Box- Rs.1000/-.
  A discount is given 10% of total amount if tickets are purchased on Cash.
  In case of credit card holders 5% discount is given."""
+
 seat=input("Type of seat (stalls/circle/upperclass/box):")
 pm=(input("Payment Mode (cash/credit card):"))
 x=[['stalls',625],['circle',750],['upperclass',850],['box',1000]]
