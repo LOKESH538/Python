e2b92e90911c5d76d3a818a47ab432d66557fdd2
LOKESH538 <69184455+LOKESH538@users.noreply.github.com>
2020-08-04 20:54:08 +0530

    Update EnergyCalcy.py

--- Assignment-1/EnergyCalcy.py
+++ Assignment-1/EnergyCalcy.py
@@ -1,8 +1,8 @@
-#Develop a program that calculates the energy needed to heat water from an initial temperature to a final temperature.
-# Your program should prompt the user to enter the amount of water in kilograms and the initial and final temperatures of the water.
-# The formula to compute the energy is
-#Q = M * (finalTemperature – initialTemperature) * 4184.
-#where M is the weight of water in kilograms, temperatures are in degrees Celsius,  and energy Q is measured in joules.
+"""Develop a program that calculates the energy needed to heat water from an initial temperature to a final temperature.
+ Your program should prompt the user to enter the amount of water in kilograms and the initial and final temperatures of the water.
+ The formula to compute the energy is
+Q = M * (finalTemperature – initialTemperature) * 4184.
+where M is the weight of water in kilograms, temperatures are in degrees Celsius,  and energy Q is measured in joules."""
 
 M=int(input("Weight of water (in Kg):"))
 finalTemp=int(input("finalTemp (in degree Celsius)"))
