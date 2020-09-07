d190e48b0bc57d47304c0717af051f52604b112b
LOKESH538 <69184455+LOKESH538@users.noreply.github.com>
2020-09-07 20:43:21 +0530

    Update 2.py

--- Assignment-3/2.py
+++ Assignment-3/2.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import matplotlib as plt
 plt.rcParams.update({'figure.max_open_warning': 0})
-df = pd.read_csv("mobile_cleaned-1549119762886.csv")
-for col in df.columns:
+data = pd.read_csv("mobile_cleaned-1549119762886.csv")
+for col in data.columns:
     if(col != 'price'):
-        df.plot(x='price',y=col)
+        data.plot(x='price',y=col)
